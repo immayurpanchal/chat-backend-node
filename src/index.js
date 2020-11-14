a2021036a655f0775a974964c5b48e9aab162bad
Mayur Panchal <mayurpanchal002@gmail.com>
2020-11-14 16:50:11 +0530

    added a sample /home route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 3000;
 
+app.get('/home', (req, res) => {
+	res.send('Home Screen');
+});
+
 app.listen(port, () => {
 	console.log(`Server is up and running 🔥 on port ${port}`);
 });
